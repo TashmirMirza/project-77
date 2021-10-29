d1c59f7a44263219460781f77c7cbd21163841a7
TashmirMirza <84456270+TashmirMirza@users.noreply.github.com>
2021-10-29 20:16:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var first="Tashmir";
+var last="Mirza";
+console.log(first.replace("Tashmir","Tanazza"));
+console.log(first.toUpperCase());
+console.log(last.toLowerCase());
+console.log(Math.max(4366145724564,645735643254));
+console.log(Math.min(7652424646789,9767545768));
+console.log(Math.pow(27312647,2));
+console.log(Math.sqrt(625,25));
+console.log(Math.round(263263453.763452643));
